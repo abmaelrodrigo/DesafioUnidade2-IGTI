18cb4fff86c57af601944f431801be7dc82dfcec
Abmael Rodrigo <abmaelrodrigo@outlook.com>
2020-10-13 19:06:12 -0300

    tratamento de erros

--- routers/grades.js
+++ routers/grades.js
@@ -7,9 +7,29 @@ const { readFile, writeFile } = fs;
 router.post('/', async (req, res, next) => {
   try {
     let grade = req.body;
+
+    //validação dos elementos recebidos:
+    if (
+      !grade.student ||
+      !grade.subject ||
+      !grade.type ||
+      grade.value == null
+    ) {
+      throw new Error(
+        'Os campos student, subject, type e value são obrigatórios.'
+      );
+    }
+
     const data = JSON.parse(await readFile('gradesTest.json'));
 
-    grade = { id: data.nextId++, ...grade, timestamp: new Date() };
+    grade = {
+      id: data.nextId++,
+      student: grade.student,
+      subject: grade.subject,
+      type: grade.type,
+      timestamp: new Date(),
+    };
+
     data.grades.push(grade);
 
     await writeFile('gradesTest.json', JSON.stringify(data, null, 2));
@@ -34,11 +54,16 @@ router.get('/', async (req, res, next) => {
 });
 
 router.get('/:id', async (req, res, next) => {
+  let receivedID = req.params.id;
   try {
     const data = JSON.parse(await readFile('gradesTest.json'));
+
     const grade = data.grades.find(
       (grade) => grade.id === parseInt(req.params.id)
     );
+    if (typeof grade === 'undefined') {
+      throw new Error('ID não encontrado');
+    }
     res.send(grade);
     logger.info('GET /grades/:id');
   } catch (err) {
@@ -49,12 +74,20 @@ router.get('/:id', async (req, res, next) => {
 router.delete('/:id', async (req, res, next) => {
   try {
     const data = JSON.parse(await readFile('gradesTest.json'));
+
+    const userID = data.grades.find(
+      (grade) => grade.id === parseInt(req.params.id)
+    );
+    if (typeof userID === 'undefined') {
+      throw new Error('ID não encontrado');
+    }
+
     data.grades = data.grades.filter(
       (grade) => grade.id !== parseInt(req.params.id)
     );
     await writeFile('gradesTest.json', JSON.stringify(data, null, 2));
     res.send('Usuário deletado com sucesso!');
-    res.end();
+
     logger.info(`GET /grades/:id - ${req.params.id}`);
   } catch (err) {
     next(err);
@@ -64,13 +97,33 @@ router.delete('/:id', async (req, res, next) => {
 router.put('/', async (req, res, next) => {
   try {
     let grade = req.body;
+    if (
+      grade.id == null ||
+      !grade.student ||
+      !grade.subject ||
+      !grade.type ||
+      grade.value == null
+    ) {
+      throw new Error(
+        'Os campos id, student, subject, type e value são obrigatórios.'
+      );
+    }
 
     const data = JSON.parse(await readFile('gradesTest.json'));
     const index = data.grades.findIndex(
       (gradeIndex) => gradeIndex.id === grade.id
     );
 
-    data.grades[index] = { ...grade, timestamp: new Date() };
+    if (index === -1) {
+      throw new Error('Registro não encontrado');
+    }
+    data.grades[index] = {
+      id: grade.id,
+      student: grade.student,
+      subject: grade.subject,
+      type: grade.type,
+      timestamp: new Date(),
+    };
 
     await writeFile('gradesTest.json', JSON.stringify(data, null, 2));
     res.send(grade);
@@ -85,15 +138,22 @@ router.post('/mediaStudent', async (req, res, next) => {
     let grade = req.body;
     let sumGrades = 0;
 
+    if (!grade.student || !grade.subject) {
+      throw new Error('Os campos student e  subject são obrigatórios.');
+    }
+
     const data = JSON.parse(await readFile('gradesTest.json'));
 
-    const index = data.grades.find((student) => {
+    const findedGrades = data.grades.find((student) => {
       if (student.student === grade.student) {
         if (student.subject === grade.subject) {
           sumGrades = student.value + sumGrades;
         }
       }
     });
+
+    console.log(typeof findedGrades);
+
     res.send(
       `Soma das notas da disciplina ${grade.subject} do(a) aluno(a) ${grade.student}: ${sumGrades}`
     );
@@ -110,9 +170,13 @@ router.post('/mediaSubject', async (req, res, next) => {
     let sumValues = 0;
     let countRegisters = 0;
 
+    if (!grade.subject || !grade.type) {
+      throw new Error('Os campos subject e  type são obrigatórios.');
+    }
+
     const data = JSON.parse(await readFile('gradesTest.json'));
 
-    const index = data.grades.find((subject) => {
+    const findedSubject = data.grades.find((subject) => {
       if (subject.subject === grade.subject) {
         if (subject.type === grade.type) {
           countRegisters++;
@@ -137,12 +201,14 @@ router.post('/bestGrades', async (req, res, next) => {
   try {
     let grade = req.body;
     let gradesArray = [];
-    let sumValues = 0;
-    let countRegisters = 0;
+
+    if (!grade.subject || !grade.type) {
+      throw new Error('Os campos subject e  type são obrigatórios.');
+    }
 
     const data = JSON.parse(await readFile('gradesTest.json'));
 
-    const index = data.grades.find((subject) => {
+    const findedBestGrade = data.grades.find((subject) => {
       if (subject.subject === grade.subject) {
         if (subject.type === grade.type) {
           gradesArray.push(parseInt(subject.value));
